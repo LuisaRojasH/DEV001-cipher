798ab16d35bea10f21ae65479ec9edac2ddf6b0c
LuisaRojasH <luisarojas182@hotmail.com>
2022-10-18 12:11:36 -0500

    final del proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
 const cipher = {
-  // ...
-};
+  encode: function (desplazamiento, mensaje) {
+    //TypeError solo numeros para el desplazamiento y solo letras para el mensaje
+   if(typeof desplazamiento != "number" || desplazamiento === 0){
+    throw new TypeError("solo numeros")
+  } 
+   if(typeof mensaje !== "string"){
+    throw new TypeError("solo letras")
+  } 
+  //declarar variable string donde guardar el resultado de la funcion
+    let string = ""
+  //ciclo for para recorrer el mensaje y obtener el numero correspondiente del codigo Ascii
+    for (let index = 0; index < mensaje.length; index++) {
+          let charA = mensaje[index].charCodeAt();
+          if(charA >= 65 && charA <= 90){ //si el codigo ascii esta entre 65 y 90 aplicar la ecuacion
+            let cesar = (parseInt(charA) - 65 + desplazamiento)%26 + 65
+            let desdeCodA = String.fromCharCode(cesar) //cambiar los nuevos numeros ascii a caracteres
+            string += desdeCodA
+          }
+          else
+           string += mensaje[index] //si no esta entre 65 y 90 se deja igual al mensaje original       
+     } 
+     return string 
+    },
 
+  decode: function (desplazamiento, mensaje) {
+    if(typeof desplazamiento !== "number" || desplazamiento === 0){
+      throw new TypeError("solo numeros")
+    } 
+    if(typeof mensaje !== "string"){
+      throw new TypeError("solo letras")
+    } 
+    let string2 = ""
+    for (let i = 0; i<mensaje.length; i++) {
+          let ascii = mensaje[i].charCodeAt()
+          if(ascii >= 65 && ascii <= 90){
+            let cesar = (parseInt(ascii)+ 65 - desplazamiento)%26 + 65
+            let desdeAscii = String.fromCharCode(cesar)
+            string2 += desdeAscii
+          }
+          else string2 += mensaje[i]
+    } 
+    return string2
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
+//trae el boton cifrar y le da una funcion
+document.getElementById("code").addEventListener("click", resultado)
+function resultado() {
+    //trae el valor del mensaje y el numero para el desplazamiento ingresado por el usuario
+    //y muestra el resultado de encode en la interfaz 
+    let mensaje = document.getElementById("mensaje").value
+    let desplazamiento = parseInt(document.getElementById("numcod").value);
+    document.getElementById("codigo").innerHTML = cipher.encode(desplazamiento, mensaje)
+    }
+ //trae el boton descifrar y le da una funcion 
+document.getElementById("decode").addEventListener("click", resultado2);
+    function resultado2() {
+    //trae el valor del mensaje codificado y el numero para el desplazamiento ingresado por el usuario
+    //y muestra el resultado de decode en la interfaz
+    let mensaje = document.getElementById("mensaje2").value
+    let desplazamiento = parseInt(document.getElementById("numdecod").value)
+    document.getElementById("codigo2").innerHTML = cipher.decode(desplazamiento, mensaje)
+    }
 
-console.log(cipher);
